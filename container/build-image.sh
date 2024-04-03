ea9b18dff8a7858fddd8c173448212aa2d39f41f
savoptik <sav70792@gmail.com>
2024-04-03 11:48:38 +0300

    update env

--- container/build-image.sh
+++ container/build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -efu
 
 podman build --force-rm=true --build-arg cuid="$(id -u)" --build-arg cgid="$(id -g)" \
-	-t angryrunt/programming-lern-container . 2>&1 | tee ./build.log
+	-t angryrunt/programming-lern-container container/ 2>&1 | tee ./build.log
--- env.sh
+++ env.sh
@@ -3,8 +3,24 @@
 WORKDIR="$(dirname $(readlink -e $0))"
 
 if [ ! -f /run/.containerenv ]; then
+uid=500
+    gid=500
+    subuidSize=$(( $(podman info --format "{{ range \
+       .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+    subgidSize=$(( $(podman info --format "{{ range \
+       .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+    
+
     podman run -it --rm --name $(mktemp -u angryrunt-programming-lern-container-XXXXXX) \
         --security-opt seccomp=unconfined --cap-add all \
+              --user $uid:$gid \
+      --uidmap 0:1:$uid \
+      --uidmap $uid:0:1 \
+      --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+      --gidmap 0:1:$gid \
+  --gidmap $gid:0:1 \
+  --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
            -v "$WORKDIR:/host" \
            --cap-add SYS_PTRACE \
            angryrunt/programming-lern-container bash $@
